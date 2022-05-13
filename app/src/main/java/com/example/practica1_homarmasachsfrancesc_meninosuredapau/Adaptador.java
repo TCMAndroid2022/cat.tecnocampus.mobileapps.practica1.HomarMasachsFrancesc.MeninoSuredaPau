e83079bbdbda41d1071a73e894ba8a6b0aa3b374
Pmenino <pmenino@edu.tecnocampus.cat>
2022-05-13 21:57:10 +0200

    Pues media práctica

---
+++ app/src/main/java/com/example/practica1_homarmasachsfrancesc_meninosuredapau/Adaptador.java
@@ -0,0 +1,56 @@
+package com.example.practica1_homarmasachsfrancesc_meninosuredapau;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class Adaptador extends RecyclerView.Adapter<Adaptador.MyViewHolder> {
+
+    Context context;
+    String[] expositors, tipologia, nStand;
+
+    public Adaptador(Context context, String[] expositors, String[] tipologia, String[] nStand) {
+        this.context = context;
+        this.expositors = expositors;
+        this.tipologia = tipologia;
+        this.nStand = nStand;
+    }
+
+    @NonNull
+    @Override
+    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        LayoutInflater layoutInflater = LayoutInflater.from(context);
+        View view = layoutInflater.inflate(R.layout.layout, parent, false);
+        return new MyViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
+        holder.expositorsTxt.setText(expositors[position]);
+        holder.tipologiaTxt.setText(tipologia[position]);
+        holder.nStandTxt.setText(nStand[position]);
+    }
+
+    @Override
+    public int getItemCount() {
+        return expositors.length;
+    }
+
+    public class MyViewHolder extends RecyclerView.ViewHolder {
+
+        TextView expositorsTxt, tipologiaTxt, nStandTxt;
+
+        public MyViewHolder(@NonNull View itemView) {
+            super(itemView);
+            expositorsTxt = itemView.findViewById(R.id.expositorsTxt);
+            tipologiaTxt = itemView.findViewById(R.id.tipologiaTxt);
+            nStandTxt = itemView.findViewById(R.id.nStandTxt);
+        }
+    }
+
+}
--- app/src/main/java/com/example/practica1_homarmasachsfrancesc_meninosuredapau/MainActivity.java
+++ app/src/main/java/com/example/practica1_homarmasachsfrancesc_meninosuredapau/MainActivity.java
@@ -1,14 +1,28 @@
 package com.example.practica1_homarmasachsfrancesc_meninosuredapau;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    RecyclerView llistaExpositors;
+    String[] expositors, tipologia, nStand;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        llistaExpositors = findViewById(R.id.llistaExpositors);
+        expositors = getResources().getStringArray(R.array.expositors);
+        tipologia = getResources().getStringArray(R.array.tipologia);
+        nStand = getResources().getStringArray(R.array.nStand);
+
+        Adaptador adaptador = new Adaptador(this, expositors, tipologia, nStand);
+        llistaExpositors.setAdapter(adaptador);
+        llistaExpositors.setLayoutManager(new LinearLayoutManager(this));
     }
 }
